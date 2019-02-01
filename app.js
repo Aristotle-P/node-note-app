18f573f45d969fa2b08e46370ff86a7e339e6d99
Aristotle Poultney <aristotlepoultney@gmail.com>
2019-02-01 18:13:23 -0500

    finished simple note app

--- app.js
+++ app.js
@@ -1,36 +1,66 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
-const command = argv._[0];
-console.log('Command: ', command);
-console.log('Yargs', argv);
+const titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+};
+const bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+};
 
-if (command === 'add') {
-  const note = notes.addNote(argv.title, argv.body);
-  if (note) {
-    notes.logNote(note);
-  } else {
-    console.log('That note already exists');
-  }
-} else if (command === 'list') {
-  notes.getAll();
-} else if (command === 'read') {
-  const note = notes.getNote(argv.title);
-  if (note) {
-    notes.logNote(note);
-  } else {
-    console.log('Note not found');
-  }
-} else if (command === 'delete') {
-  const noteDeleted = notes.deleteNote(argv.title);
-  const message = noteDeleted ? 'Note deleted' : 'Note not found';
-  console.log(message);
-} else {
-  console.log('Command not recognized');
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Reads indivudual note', {
+    title: titleOptions
+  })
+  .command('delete', 'Deletes note', {
+    title: titleOptions
+  })
+  .help().argv;
+const command = argv._[0];
+
+switch (command) {
+  case 'add':
+    const note = notes.addNote(argv.title, argv.body);
+    if (note) {
+      notes.logNote(note);
+    } else {
+      console.log('That note already exists');
+    }
+    break;
+
+  case 'read':
+    const readNote = notes.getNote(argv.title);
+    if (readNote) {
+      notes.logNote(readNote);
+    } else {
+      console.log('Note not found');
+    }
+    break;
+
+  case 'list':
+    const allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s)`);
+    allNotes.forEach(note => notes.logNote(note));
+    break;
+
+  case 'delete':
+    const noteDeleted = notes.deleteNote(argv.title);
+    const message = noteDeleted ? 'Note deleted' : 'Note not found';
+    console.log(message);
+    break;
+
+  default:
+    console.log('Command not found');
 }
